ebca3a21a385b392922148a0b4944ae501e03f06
stryzhka <voditel.vlad@yandex.ru>
2024-04-15 17:58:19 +0300

    changing questions

--- public_html/js/data.js
+++ public_html/js/data.js
@@ -1,19 +1,27 @@
 class Question{
-    constructor(number, title, answers, trueAnswer){
+    constructor(number, type, title, answers, trueAnswer, viewed, answered){
         this.number = number;
+        this.type = type;
         this.title = title;
         this.answers = answers;
         this.trueAnswer = trueAnswer;
+        this.viewed = viewed;
+        this.answered = answered;
     }
     checkAnswer(userAnswer){
         return userAnswer === this.trueAnswer;
     }
 }
 
-let question1 = new Question(1, "Стружки крутая группа?", ["да", "нет"], 0);
-let question2 = new Question(1, "Структурная единица почки?", ["нефлон", "тейлон", "полиуретан"], 2);
+let question1 = new Question(1, "radio", "Стружки крутая группа?", ["да", "нет"], 0, false, false);
+let question2 = new Question(2, "radio", "Структурная единица почки?", ["нефлон", "тейлон", "полиуретан"], 2, false, false);
+let question3 = new Question(3, "checkbox", "Кто насиловал Членина в жопу", ["Сралин", "Срыня", "Бидон"], 1, false, false);
+let question4 = new Question(4, "radio", "Кто автор видео тамбовский дед?", ["Хова", "Мэд", "Нифедыч", "itpedia", "vjlink", "Игорь гофман"], 1, false, false);
+let question5 = new Question(5, "radio", "Что делать в случае ядерного взрыва?", ["Убегать", "Кормить собак", "В аквапарке реально охуенно", "х^2"], 1, false, false);
+let question6 = new Question(6, "select", "Сво", ["Посхалко", "7-8", "Наумов Саша"], 1, false, false);
+let question7 = new Question(7, "checkbox", "Сво", ["Сво"], 1, false, false);
 
-let questions = [question1, question2];
+let questions = [question1, question2, question3, question4, question5, question6, question7];
 
 export default questions;
 
--- public_html/js/testing.js
+++ public_html/js/testing.js
@@ -1,9 +1,73 @@
 import questions from './data.js';
 
-let currentQuestion = 1;
+let forwardButton = document.getElementById("nextQuestion");
+let prevButton = document.getElementById("prevQuestion");
+let tabDiv = document.getElementById("tabs");
 
-let nextQuestionButton = document.getElementById("nextQuestion");
 
-let questionForm = document.getElementById("questionForm");
-questionForm.innerHTML.append('<div><label><input type="radio" name="1"/>Нефлон</label></div>');
-questionForm.append('<div><label><input type="radio" name="1"/>123</label></div>');
+let currentQuestion = 0;
+
+function renderTabs(){
+    tabDiv.innerHTML = "";
+    for (let i = 0; i < questions.length; i++){
+        let htmlAppend;
+        if (questions[i].viewed){
+            console.log("True!");
+            htmlAppend = `<div class="tab-readed"></div>`;
+        }
+        else{
+            console.log("False!");
+            htmlAppend = `<div class="tab-unactive"></div>`;
+        }
+        tabDiv.insertAdjacentHTML('beforeend', htmlAppend);
+        
+    }
+    
+}
+
+function renderQuestion(){
+    let questionNumberP = document.getElementById("questionNumber");
+    let questionTitle = document.getElementsByClassName("header-theme")[0];
+    questionNumberP.innerHTML = `Вопрос ${questions[currentQuestion].number}`;
+    questionTitle.innerHTML = questions[currentQuestion].title;
+    let questionForm = document.getElementById("questionForm");
+    questionForm.innerHTML = "";
+    for (let i = 0; i < questions[currentQuestion].answers.length; ++i){
+        let htmlAppend = `<div>
+                                    <label>
+                                        <input type="${questions[currentQuestion].type}" name="1"/>
+                                        ${questions[currentQuestion].answers[i]}
+                                    </label>
+                                </div>`;
+        questionForm.insertAdjacentHTML('beforeend', htmlAppend);
+    }
+    //
+    questions[currentQuestion].viewed = true;
+    renderTabs();
+    
+}
+
+function changeQuestionForward(){
+    if (currentQuestion < questions.length-1)
+        currentQuestion++;
+    console.log(currentQuestion, questions.length);
+    renderQuestion();
+
+    
+}
+
+function changeQuestionBackward(){
+    if (currentQuestion != 0)
+        currentQuestion--;
+    renderQuestion();
+    
+    
+}
+
+
+
+forwardButton.onclick = changeQuestionForward;
+prevButton.onclick = changeQuestionBackward;
+
+
+renderQuestion();
